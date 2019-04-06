1f0522ad412aa03f7195c6f2907262dd7f1e8c7b
Jay Mendelsohn <jay.a.mendelsohn@gmail.com>
2019-04-06 11:47:45 -0400

    Add commenting

--- view-post.php
+++ view-post.php
@@ -1,104 +1,93 @@
  <?php
-require_once 'lib/common.php';
-require_once 'lib/view-post.php';
 
-// Get the post ID
-if (isset($_GET['post_id']))
+/**
+ * Retrieves a single post
+ * 
+ * @param PDO $pdo
+ * @param integer $postId
+ * @throws Exception
+ */
+function getPostRow(PDO $pdo, $postId)
 {
-    $postId = $_GET['post_id'];
-}
-else
-{
-    // So we always have a post ID var defined
-    $postId = 0;
-}
+    $stmt = $pdo->prepare(
+        'SELECT
+            title, created_at, body
+        FROM
+            post
+        WHERE
+            id = :id'
+    );
+    if ($stmt === false)
+    {
+        throw new Exception('There was a problem preparing this query');
+    }
+    $result = $stmt->execute(
+        array('id' => $postId, )
+    );
+    if ($result === false)
+    {
+        throw new Exception('There was a problem running this query');    
+    }
 
-// Connect to the database, run a query, handle errors
-$pdo = getPDO();
-$row = getPostRow($pdo, $postId);
+    // Let's get a row
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
-// If the post does not exist, let's deal with that here
-if (!$row)
-{
-    redirectAndExit('index.php?not-found=1');
-}
-
-$errors = null;
-if ($_POST)
-{
-	$commentData = array(
-		'name' => $_POST['comment-name'],
-		'website' => $_POST['comment-website'],
-		'text' => $_POST['comment-text'],
-	);
-
-	$errors = addCommentToPost(
-		$pdo,
-		$postId,
-		$commentData
-	);
-
-	// If there are no errors, redirect back to self and redisplay
-	if (!$errors)
-	{
-		redirectAndExit('view-post.php?post_id=' . $postId);
-	}
+    return $row;
 }
 
-else
+/**
+ * Writes a comment to a particular post
+ *
+ * @param PDO $pdo
+ * @param integer $postId
+ * @param array $commentData
+ * @return array
+ */
+function addCommentToPost(PDO $pdo, $postId, array $commentData)
 {
-	$commentData = array(
-		'name' => '',
-		'website' => '',
-		'text' => '',
-	);
+    $errors = array();
+
+    //Do some validation
+    if (empty($commentData['name']))
+    {
+        $errors['name'] = 'A name is required';
+    }
+    if (empty($commentData['text']))
+    {
+        $errors['text'] = 'A comment is required';
+    }
+
+    // If error free, try to write the comment
+    if (!$errors)
+    {
+        $sql = "
+            INSERT INTO
+                comment
+            (name, website, text, created_at, post_id)
+            VALUES(:name, :website, :text, :created_at, :post_id)
+        ";
+        $stmt = $pdo->prepare($sql);
+        if ($stmt === false)
+        {
+            throw new Exception('Cannot prepare statement to insert comment');
+        }
+
+        $result = $stmt->execute(
+            array_merge($commentData, array('post_id' => $postId, 
+            'created_at' => getSqlDateForNow(), )
+            )
+        );
+
+        if ($result === false)
+        {
+            //@todo This renders a database-level message to the user, fix this
+            $errorInfo = $stmt->errorInfo();
+            if ($errorInfo)
+            {
+                $errors[] = $errorInfo[2];
+            }
+        }
+    }
+
+    return $errors;
 }
-
-// Swap carriage returns for paragraph breaks
-$bodyText = htmlEscape($row['body']);
-$paraText = str_replace("\n", "</p><p>", $bodyText);
-?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>
-            A blog application |
-            <?php echo htmlEscape($row['title']) ?>
-        </title>
-        <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
-    </head>
-    <body>
-        <?php require 'templates/title.php' ?>
-
-        <h2>
-            <?php echo htmlEscape($row['title']) ?>
-        </h2>
-        <div>
-            <?php echo convertSqlDate($row['created_at']) ?>
-        </div>
-        <p>
-            <?php // This is already escaped, so doesn't need further escaping ?>
-            <?php echo $paraText ?>
-        </p>
-
-        <h3><?php echo countCommentsForPost($postId) ?> comments</h3>
-
-        <?php foreach (getCommentsForPost($postId) as $comment): ?>
-            <?php // For now, we'll use a horizontal rule-off to split it up a bit ?>
-            <hr />
-            <div class="comment">
-                <div class="comment-meta">
-                    Comment from
-                    <?php echo htmlEscape($comment['name']) ?>
-                    on
-                    <?php echo convertSqlDate($comment['created_at']) ?>
-                </div>
-                <div class="comment-body">
-                    <?php echo htmlEscape($comment['text']) ?>
-                </div>
-            </div>
-        <?php endforeach ?>
-
-        <?php require 'templates/comment-form.php' ?>
-    </body>
-</html>
