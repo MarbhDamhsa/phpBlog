016ca7ea1aa689cd3feecaa447f6664d527155b6
Jay Mendelsohn <jay.a.mendelsohn@gmail.com>
2019-04-05 22:37:13 -0400

    Update view-post.php

--- view-post.php
+++ view-post.php
@@ -1,36 +1,71 @@
  <?php
+require_once 'lib/common.php';
+require_once 'lib/view-post.php';
 
-/**
- * Retrieves a single post
- * 
- * @param PDO $pdo
- * @param integer $postId
- * @throws Exception
- */
-function getPostRow(PDO $pdo, $postId)
+// Get the post ID
+if (isset($_GET['post_id']))
 {
-    $stmt = $pdo->prepare(
-        'SELECT
-            title, created_at, body
-        FROM
-            post
-        WHERE
-            id = :id'
-    );
-    if ($stmt === false)
-    {
-        throw new Exception('There was a problem preparing this query');
-    }
-    $result = $stmt->execute(
-        array('id' => $postId, )
-    );
-    if ($result === false)
-    {
-        throw new Exception('There was a problem running this query');    
-    }
+    $postId = $_GET['post_id'];
+}
+else
+{
+    // So we always have a post ID var defined
+    $postId = 0;
+}
 
-    // Let's get a row
-    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+// Connect to the database, run a query, handle errors
+$pdo = getPDO();
+$row = getPostRow($pdo, $postId);
 
-    return $row;
-} 
+// If the post does not exist, let's deal with that here
+if (!$row)
+{
+    redirectAndExit('index.php?not-found=1');
+}
+
+// Swap carriage returns for paragraph breaks
+$bodyText = htmlEscape($row['body']);
+$paraText = str_replace("\n", "</p><p>", $bodyText);
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            A blog application |
+            <?php echo htmlEscape($row['title']) ?>
+        </title>
+        <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+    </head>
+    <body>
+        <?php require 'templates/title.php' ?>
+
+        <h2>
+            <?php echo htmlEscape($row['title']) ?>
+        </h2>
+        <div>
+            <?php echo convertSqlDate($row['created_at']) ?>
+        </div>
+        <p>
+            <?php // This is already escaped, so doesn't need further escaping ?>
+            <?php echo $paraText ?>
+        </p>
+
+        <h3><?php echo countCommentsForPost($postId) ?> comments</h3>
+
+        <?php foreach (getCommentsForPost($postId) as $comment): ?>
+            <?php // For now, we'll use a horizontal rule-off to split it up a bit ?>
+            <hr />
+            <div class="comment">
+                <div class="comment-meta">
+                    Comment from
+                    <?php echo htmlEscape($comment['name']) ?>
+                    on
+                    <?php echo convertSqlDate($comment['created_at']) ?>
+                </div>
+                <div class="comment-body">
+                    <?php echo htmlEscape($comment['text']) ?>
+                </div>
+            </div>
+        <?php endforeach ?>
+    </body>
+</html>
