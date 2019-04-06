3e903f68b057d94e7b9b306449c548b485de2056
Jay Mendelsohn <jay.a.mendelsohn@gmail.com>
2019-04-05 22:37:50 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php
+ <?php
 require_once 'lib/common.php';
 
 // Connect to the database, run a query, handle errors
@@ -11,40 +11,45 @@ $stmt = $pdo->query(
     ORDER BY
         created_at DESC'
 );
-
 if ($stmt === false)
 {
     throw new Exception('There was a problem running this query');
 }
 
+$notFound = isset($_GET['not-found']);
+
 ?>
 <!DOCTYPE html>
 <html>
-    
     <head>
         <title>A blog application</title>
         <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
     </head>
-    
     <body>
-     <?php require 'templates/title.php' ?>
+        <?php require 'templates/title.php' ?>
+
+        <?php if ($notFound): ?>
+            <div style="border: 1px solid #ff6666; padding: 6px;">
+                Error: cannot find the requested blog post
+            </div>
+        <?php endif ?>
 
         <?php while ($row = $stmt->fetch(PDO::FETCH_ASSOC)): ?>
             <h2>
-                <?php echo htmlspecialchars($row['title'], ENT_HTML5, 'UTF-8') ?>
+                <?php echo htmlEscape($row['title']) ?>
             </h2>
-
             <div>
-                <?php echo $row['created_at'] ?>
+                <?php echo convertSqlDate($row['created_at']) ?>
+                
+                (<?php echo countCommentsForPost($row['id']) ?> comments)
             </div>
-            
             <p>
-                <?php echo htmlspecialchars($row['body'], ENT_HTML5, 'UTF-8') ?>
+                <?php echo htmlEscape($row['body']) ?>
             </p>
-            
             <p>
-                <a href="view-post.php?post_id=<?php echo $row['id'] ?>"
-                    >Read more...</a>
+                <a
+                    href="view-post.php?post_id=<?php echo $row['id'] ?>"
+                >Read more...</a>
             </p>
         <?php endwhile ?>
 
